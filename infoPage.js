1cf6b940d448b4477cb593fb0d216d7727f012bf
danielmaxsiegel <danielmaxsiegel@gmail.com>
2020-08-22 13:57:05 -0700

    add a few new sites, and add the start of classifying sites by industry

--- infoPage.js
+++ infoPage.js
@@ -3,10 +3,10 @@ var sites = getSites();
 var table = '<table>';
 
 for (i = 0; i < sites.length; ++i) {
-  console.log(sites[i].name);
   table += '<tr><td>';
   table += '<a href="https://' + sites[i].site + '" target="_blank">';
   table += sites[i].name;
+  // table += getIndustryEmoji(sites[i].industry);
   table += '</a>';
   table += '</td></tr>';
 }
@@ -17,15 +17,16 @@ document.getElementById("table-div").innerHTML = table;
 
 function getSites() {
   return [
-    {name: "60°N 95°W", site: "60n95w.com"},
-    {name: "3C Underwear", site: "www.3cunderwear.ca"},
-    {name: "Abaka", site: "abaka.ca"},
-    {name: "Alberta Apparel", site: "albertaapparel.com"},
+    {name: "60°N 95°W", site: "60n95w.com", industry: "fashion"},
+    {name: "3C Underwear", site: "www.3cunderwear.ca", industry: "fashion"},
+    {name: "Abaka", site: "abaka.ca", industry: "fashion"},
+    {name: "Alberta Apparel", site: "albertaapparel.com", industry: "fashion"},
     {name: "Algoma Steel", site: "www.algoma.com"},
-    {name: "Arc'Teryx", site: "arcteryx.com"},
-    {name: "Aritzia", site: "www.aritzia.com"},
-    {name: "Arturo Denim", site: "www.arturodenim.ca"},
-    {name: "The Beer Store", site: "www.thebeerstore.ca"},
+    {name: "Arc'Teryx", site: "arcteryx.com", industry: "fashion"},
+    {name: "Aritzia", site: "www.aritzia.com", industry: "fashion"},
+    {name: "Arturo Denim", site: "www.arturodenim.ca", industry: "pants"},
+    {name: "Bearface Whiskey", site: "www.bearfacewhisky.com", industry: "spirits"},
+    {name: "The Beer Store", site: "www.thebeerstore.ca", industry: "beer"},
     {name: "Bombardier", site: "www.bombardier.com"},
     {name: "Bonnetier", site: "eng.bonnetier.com"},
     {name: "Bon Vivant", site: "bonvivantshop.com"},
@@ -43,6 +44,7 @@ function getSites() {
     {name: "Jack Victor", site: "www.jackvictor.com"},
     {name: "J.P. Wisers", site: "www.jpwisers.com"},
     {name: "Kit And Ace", site: "www.kitandace.com"},
+    {name: "La Canadienne Shoes", site: "www.lacanadienneshoes.com"},
     {name: "LCBO", site: "www.lcbo.com"},
     {name: "Local Laundry", site: "locallaundry.ca"},
     {name: "Lolë", site: "www.lolelife.com"},
@@ -57,6 +59,7 @@ function getSites() {
     {name: "Paderno", site: "www.paderno.com"},
     {name: "Pajar", site: "ca.pajar.com"},
     {name: "Point Zero", site: "www.point-zero.com"},
+    {name: "Province of Canada", site: "provinceofcanada.com"},
     {name: "Redpath Sugar", site: "www.redpathsugar.com"},
     {name: "Rolled Alloys", site: "www.rolledalloys.com"},
     {name: "Roots", site: "www.roots.com"},
@@ -69,8 +72,20 @@ function getSites() {
     {name: "Stede", site: "stede.ca"},
     {name: "String Theory", site: "stringtheory.co"},
     {name: "The Unbranded Brand", site: "theunbrandedbrand.com"},
+    {name: "Unibroue", site: "www.unibroue.com"},
     {name: "Wine Rack", site: "www.winerack.com"},
     {name: "Wings + Horns", site: "wingsandhorns.com"},
     {name: "Zen Nomad", site: "zennomad.ca"}
   ];
+}
+
+function getIndustryEmoji(industry) {
+  switch (industry) {
+    case 'beer':
+      return '🍺';
+    case 'fashion':
+      return '👕';
+    case 'spirits':
+      return '🥃';
+  }
 }
